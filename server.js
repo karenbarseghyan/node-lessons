905d23b3937a988858d949e4ab8760880c217724
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-11 14:51:46 +0400

    Server call feature change

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hi!')
 })
 
-app.listen(3000, () => console.log('Server ready'))
+const server = app.listen(3000, () => console.log('Server ready'))
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated')
+    })
+})
