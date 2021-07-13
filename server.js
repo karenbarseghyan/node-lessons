e8463584b219097e59e21138dd96552db2e73fca
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-13 12:31:54 +0400

    EventEmitter

---
+++ server.js
@@ -0,0 +1,29 @@
+const {EventEmitter} = require('events');
+
+const logger = new EventEmitter();
+
+const users = [];
+const messages = [];
+
+logger.on('message', (...msg)=> {
+    messages.push(msg);
+    console.log(`New message [${msg}]`);
+})
+
+logger.on('login', (...name)=> {
+    users.push(name);
+    console.log(`New user [${name}]`);
+})
+
+logger.on('getUsers', ()=> {
+    users[0].forEach((item)=>console.log(item));
+})
+
+logger.on('getMessages', ()=> {
+    messages[0].forEach((item)=>console.log(item));
+})
+
+logger.emit('message','Hello World', 'Good-By');
+logger.emit('login','Karen', "Ruben");
+logger.emit('getUsers');
+logger.emit('getMessages');
