f63e214d54d92ddf45ed978a395288b6b6177817
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-21 11:49:20 +0400

    Addition to csv to json

--- hmw3.js
+++ hmw3.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path')
+// const csvToJson = require('csvtojson');
 
 
 const rl = readline.createInterface({
@@ -17,7 +18,12 @@ const change = (file, extension) => {
     return path.join(path.dirname(file), basename + extension)
 }
 change('C:\\Users\\Karen\\Desktop\\nn\\addresses.csv','.json')
-
+/*csvToJson()
+    .fromFile(path.join(__dirname, 'addresses.csv'))
+    .then(jsonObj => {
+        console.log(jsonObj);
+        fs.writeFileSync(path.join(__dirname, 'addresses.json'), JSON.stringify(jsonObj, null, 2));
+    });*/
 
 // fs.readFile('C:\\Users\\Karen\\Desktop\\nn\\addresses.csv', (err, data)=> {
 //     const basename = path.basename('./addresses','.csv');
