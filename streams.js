e42a93af82fc7c21df6cc0bbea4aaeb88a1fae4f
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-28 13:32:04 +0400

    Unzip to adjacentNewFile

--- streams.js
+++ streams.js
@@ -1,18 +1,31 @@
 const fs = require("fs")
+const zlib = require("zlib")
+const path = require("path")
 
-const stream = fs.createReadStream('./main.txt', {
-    highWaterMark: 100,
-    encoding: "utf-8"
+
+let pathFunc = function getRandomString(length) {
+    length = Math.floor(Math.random() * 20);
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for ( let i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+let paths = pathFunc();
+console.log(paths);
+fs.mkdir(path.join(__dirname, `${paths}`), (err) => {
+    if(err) {
+        throw(err)
+    }
+    console.log("dir created");
+
 })
-const output = fs.createWriteStream("./writeMe.txt")
-stream.pipe(output);
 
-// stream.on('data', chunk => {
-//     output.write(chunk)
-//     console.log("writeStream");
-// })
+const input = fs.createReadStream(`./dir1/test.txt`)
+const output = fs.createWriteStream(`.//${paths}/test.gzip`)
 
-
+input.pipe(zlib.createGzip()).pipe(output)
 
 
 
